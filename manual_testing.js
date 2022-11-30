d3015a602a831f429d5d386a47ca9cea4774bb3a
Russel <mr.mang-usan@gmail.com>
2022-11-30 13:54:55 +0800

    Manual Testing for BMI Calculator

---
+++ manual_testing.js
@@ -0,0 +1,28 @@
+const {BMI, bmiInt} = require("./bmi_calculator");
+
+console.log("\nStart Manual Testing...")
+
+const testBMI = () => {
+    console.log("\nTesting BMI calculator function");
+    const expectedBMI = 31.22;
+    const result = BMI;
+    console.assert(
+        result == expectedBMI,
+        `The result ${result} does not match the expected value ${expectedBMI}`
+    );
+    if (result == expectedBMI) {console.log(`PASSED...`)};
+}
+
+const testBMIinterpretation = () => {
+    console.log("\nTesting BMI interpretation function")
+    const expectedInt = "You're Obese";
+    const result = bmiInt;
+    console.assert(
+        result == expectedInt,
+        `The result "${result}" does not match the expected value "${expectedInt}"`
+    );
+    if (result == expectedInt) {console.log(`PASSED...`)};
+}
+
+testBMI();
+testBMIinterpretation();
